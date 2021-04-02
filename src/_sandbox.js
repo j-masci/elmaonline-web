8207c4726b853bfa468681d9e042e6be0d6993d3
Joel Masci <masci.joel@gmail.com>
2021-04-02 15:11:23 -0400

    asyncProp thing (wip)

--- src/_sandbox.js
+++ src/_sandbox.js
@@ -1,17 +1,61 @@
 import React, { useEffect, useState } from 'react';
-import { useStoreState, useStoreActions } from 'easy-peasy';
+import {
+  useStoreState,
+  useStoreActions,
+  action,
+  thunk,
+  computed,
+} from 'easy-peasy';
 import Layout from './components/Layout';
+
+const ascynProp = (initialValue, getter) => {
+  return {
+    loading: false,
+    value: initialValue,
+    desiredInput: null,
+    currentInput: null,
+    setInput: action((state, payload) => {
+      state.desiredInput = state;
+    }),
+    updateState: action((state, func) => {
+      func(state);
+    }),
+    fetch: thunk(async (actions, payload, helpers) => {
+      actions.updateState(state => {
+        state.loading = true;
+      });
+
+      const value = await getter(payload);
+
+      actions.updateState(state => {
+        state.loading = false;
+      });
+    }),
+  };
+};
 
 export const store = {
   thing: 123,
+  nested: {
+    a: 10,
+    b: 20,
+  },
 };
 
 export const Comp = () => {
-  const { thing } = useStoreState(store => store.Sandbox);
+  const state = useStoreState(store => store.Sandbox);
+  const actions = useStoreActions(store => store);
+
+  console.log(actions);
+
+  useEffect(() => {
+    actions.Sandbox.shit(900);
+  });
 
   return (
     <Layout t="sandbox">
-      <div>{thing}</div>
+      <div>{state.thing}</div>
+      <div>{state.fuck}</div>
     </Layout>
   );
 };
